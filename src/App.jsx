b2f2221eb21dd310cc3a270ca3ac4974b4d86d53
kdada223 <kangh2580@naver.com>
2025-10-12 21:46:43 +0900

    공부하셈

---
+++ src/App.jsx
@@ -0,0 +1,113 @@
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import './App.css';
+import { useState } from 'react';
+import data from './data.jsx';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import DetailPage from './routes/detail.jsx';
+import { MainPage } from './routes/main.jsx';
+import axios from 'axios';
+
+function App() {
+	let [shoes, shoesState] = useState(data);
+	let navigate = useNavigate();
+	let [count, setCount] = useState(2);
+	let [isLoading, loadingState] = useState(false);
+
+	return (
+		<div className='App'>
+			<Navbar bg='dark' data-bs-theme='dark' className='Nav-Main'>
+				<Container className='Nav-Box'>
+					<Navbar.Brand href='#home'>Steam</Navbar.Brand>
+					<Nav className='me-auto'>
+						<Nav.Link
+							onClick={() => {
+								navigate('/');
+							}}>
+							Home
+						</Nav.Link>
+						<Nav.Link
+							onClick={() => {
+								navigate('/detail');
+							}}>
+							Detail
+						</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar>
+
+			<Routes>
+				<Route path='/' element={<MainPage shoes={shoes} shoesState={shoesState} count={count} setCount={setCount} isLoading={isLoading} loadingState={loadingState}></MainPage>} />
+
+				<Route path='/detail' element={<DetailPage shoes={shoes}></DetailPage>} />
+
+				<Route path='/detail/:id' element={<DetailPage shoes={shoes}></DetailPage>} />
+
+				<Route path='/about' element={<About></About>}>
+					<Route path='member' element={<div>멤버임</div>} />
+					<Route path='location' element={<div>위치정보임</div>} />
+				</Route>
+
+				<Route path='/event' element={<Event></Event>}>
+					<Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>} />
+					<Route path='two' element={<div>생일기념 쿠폰받기</div>} />
+				</Route>
+
+				<Route path='*' element={<div>없는페이지</div>} />
+			</Routes>
+		</div>
+	);
+}
+
+function Event() {
+	return (
+		<div>
+			<h4>오늘의 이벤트</h4>
+			<Outlet></Outlet>
+		</div>
+	);
+}
+
+function About() {
+	return (
+		<div>
+			<div>어바웃페이지임</div>
+			<Outlet></Outlet>
+		</div>
+	);
+}
+
+export default App;
+
+//서버 : 데이터를 요청하면 데이터를 보내주는 걸 서버라고 함.
+//서버 개발시 짜는 코드 : 누가 A요청하면 A보내주세요
+//그냥 달라는게 아니라 정확히 규격에 맞춰 요청해야함.
+//규격은 방법과 자료가 있는데 방법은 GET/POST이고 어떤자료는 URL형식으로 기입을 해주면 됨.
+//네이버 웹툰이 보고 싶다. 그러면 GET요청을 하고 comic.naver.com 으로 요청을 하면됨.
+//네이버 블로그서버에 네이버 글을 하나 발행하고 싶다? 그러면 POST요청을 하면됨 어디로? blog.naver.com 으로
+//그러면 서버에 내 블로그가 저장됨.
+//URL은 서버 만든 사람한테 물어보면 알려줌
+//서버 요청 방식을 자세히 설명하면 누가 comic.naver.com 으로 GET요청하면 웹툰 보내주쇼
+//ajax라는 걸 사용하면 새로고침 없이도 GET/POST 요청 가능
+//ajax 쓰려면 3가지 정도 방법이 있는데
+//1. XMLHttpRequest(옛날 자바스크립트 문법) 2. fetch(요즘 자바스크립트 문법) 3. axios 같은거 (외부라이브러리) 5분31초
+
+//서버로 데이터 전송 axios.post('url입력', {넣고싶은 데이터 ex) name: 'kim'})
+//get요청을 여러 곳으로 요청하고 싶을 때는
+//Promise.all([axios.get('url입력')], [axios.get('url입력')]) 그 후에 두개가 다 성공했을 때 데이터를 받고 싶어요 하면
+//Promise.all([axios.get('url입력')], [axios.get('url입력')]).then(()=>{}) 이렇게 작성
+//나눠서 작성해도 되는데 그건 둘 중 하나라도 되면 성공한게 나옴
+//원래 서버와 데이터를 주고 받을 때는 문자만 주고 받을 수 있음. array나 object자료는 못받음
+//그런데 어떻게 우리는 array 자료를 받았냐?
+//array나 object자료를 주고 받을 때 '{'name' : 'kim'}' 이렇게 따옴표를 쳐놓으면 array나 object 자료도 주고 받기 가능하다.
+//이런 자료들을 JSON 이라고 한다.
+//axios가 문자열로 도착한 JSON 파일은 array로 자동으로 바꿔줌
+
+//응용3. 버튼을 누른 직후엔 "로딩중입니다" 이런 글자를 주변에 띄우고 싶으면?
+
+// 그리고 요청이 성공하거나 실패하거나 그 후엔 "로딩중입니다" 글자를 제거해야합니다.
+
+
+//동적UI만드는법 3스텝 
+//html, css 디자인
+//UI의 현재 상태를 state로 저장
+//state에 따라서 UI가 어떻게 보일지 작성
---
+++ src/routes/detail.jsx
@@ -0,0 +1,175 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Nav } from 'react-bootstrap';
+
+function DetailPage(props) {
+	let [display, displayState] = useState(true);
+	let [numAlert, AlertState] = useState('');
+
+	let [count, countState] = useState(0);
+	let [tab, tabState] = useState(0);
+
+	useEffect(() => {
+		// 안에 사용하는 것들
+		// 어려운 연산
+		// 서버에서 데이터가져오는 작업
+		// 타이머 장착하는거
+		//useEffect에서 생명주기
+		//Mount (마운트): 컴포넌트가 처음 생성되어 DOM에 삽입될 때
+		// useEffect 실행
+
+		// Update (업데이트): 컴포넌트가 재렌더링될 때 (state나 props 변경)
+		// 이전 useEffect cleanup 실행 → 새 useEffect 실행
+
+		// Unmount (언마운트): 컴포넌트가 DOM에서 제거될 때
+		// cleanup 함수 실행
+
+		//결론 언마운트만 실행하고 싶다 useEffect안의 return에 함수 작성
+		//마운트시 1회 코드 실행 [] 대괄호 작성
+		//재렌더링마다 코드실행 그냥 useEffect안에 작성
+
+		let a = setTimeout(() => {
+			displayState(false);
+		}, 2000);
+		console.log(2);
+		return () => {
+			// 기존 타이머는 제거해주세요
+			// clearTimeout(a); //타이머 제거 함수 타이머를 만들고 변수에 저장한 다음 그 변수를 clearTimeout 이라는 함수에 집어넣으면 제거가 됨
+			//서버에서 데이터를 요청하는 코드를 썼는데 그게 2초 정도 걸린다고 가정해보자
+			//그런데 2초사이에 재렌더링이 되어버리면? 계속해서 무한 요청을 보내게 되는데 그거를 막는 방법은?
+			//기존 데이터 요청은 제거해주세요 이럴 때도 clear함수 사용하면 됨.
+			console.log(1);
+			clearTimeout(a);
+		};
+	}, []);
+	useEffect(() => {
+		//인풋에서 값을 받아와서 인풋창의 내용이 int값 즉 정수형이 아니면 AlertState를 true로 변경
+		//여기서 중요한 점!onchage라는 함수가 있는데 이게 바로 js의 자바스크립트의 input내용과 비슷하다.
+		//작성할 때 실시간으로 값을 업데이트 해서 확인해줌
+		//isNaN이라는 함수를 쓰는데 저놈은 숫자가 아닌 값을 찾아냄
+		//그러니까 isNaN안의 ()에 들어간 값이 숫자다! 그러면 false를 반환하지만 숫자가 아니면 true를 반환한다.
+		//결론! isNaN(true)면 어떻게 할래? alert창을 띄우고 문제있음을 실행한다.
+		//그런데 문제가 생길 때마다 해야하니까 디펜던시스에 input에 들어갈 내용을 체크해야ㅕ하기 떄문에 numAlert이 값을 넣어준다.
+		//또 업데이트 마다 실행해야하기때문에 [] 안에 값을 numAlert 이걸로 넣어서 확인하면될듯함. 위의 주석과 같은 내용
+		if (isNaN(numAlert) === true) {
+			alert('똑바로 입력하삼');
+		}
+	}, [numAlert]);
+
+	let { id } = useParams();
+	let findItem = props.shoes.find((x) => {
+		return x.id == id;
+	});
+
+	// id가 없거나 findItem이 없을 때 기본값 설정
+	if (!findItem) {
+		findItem = props.shoes.find((x) => x.id === 0); // id가 0인 상품을 찾기
+	}
+
+	let [fade, fadeState] = useState('');
+	useEffect(() => {
+		setTimeout(() => {
+			fadeState('end');
+		}, 50);
+		return () => {
+			fadeState('');
+		};
+	}, []);
+
+	return (
+		<div className={`container start ${fade}`}>
+			{display == true ? <div className='alert alert-warning'>2초 이내 구매시 할인</div> : null}
+			<button
+				onClick={() => {
+					countState(count + 1);
+					console.log(count);
+				}}>
+				업데이트
+			</button>
+			<div className='row'>
+				<div className='col-md-6'>
+					<img src={findItem.img} width='100%' />
+				</div>
+				<div className='col-md-6'>
+					<input
+						type='text'
+						placeholder='수량을 입력하세요'
+						onChange={(e) => {
+							{
+								AlertState(e.target.value);
+							}
+						}}
+					/>
+					<h4 className='pt-5'>{findItem.title}</h4>
+					<p>{findItem.content}</p>
+					<p>{findItem.price}</p>
+					<button className='btn btn-danger'>주문하기</button>
+				</div>
+			</div>
+			<Nav variant='tabs' defaultActiveKey='link-0'>
+				<Nav.Item>
+					<Nav.Link
+						eventKey='link-0'
+						onClick={() => {
+							tabState(0);
+						}}>
+						버튼0
+					</Nav.Link>
+					{/* 이거 눌렀을 때 state 변경해서 이거랑 연결된 창 띄워주세요~ */}
+				</Nav.Item>
+				<Nav.Item>
+					<Nav.Link
+						eventKey='link-1'
+						onClick={() => {
+							tabState(1);
+						}}>
+						버튼1
+					</Nav.Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Nav.Link
+						eventKey='link-2'
+						onClick={() => {
+							tabState(2);
+						}}>
+						버튼2
+					</Nav.Link>
+				</Nav.Item>
+			</Nav>
+			<TabContent tab={tab} />
+		</div>
+	);
+}
+
+//클린업함수에 넣으면 되야하는데 왜 안되나?
+//리액트의 automatic batching이라는 기능때문인데
+//state변경한 함수들이 근처에 있으면 하나로 합쳐서 한번만 state를 변경해줌.
+//state가 변경될 때마다 재렌더링이 되는게 아니라 마지막에 짠 state변경함수만 재렌더링을 해줌
+function TabContent({ tab }) {
+	let [fade, fadeState] = useState('');
+
+	useEffect(() => {
+		setTimeout(() => {
+			fadeState('end');
+		}, 100);
+		return () => {
+			fadeState('');
+		};
+	}, [tab]);
+	return <div className={`start ${fade}`}>{[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][tab]}</div>;
+}
+
+// function TabContent(props) {
+// 	if (props.tab == 0) {
+// 		return <div>내용0</div>;
+// 	} else if (props.tab == 1) {
+// 		return <div>내용1</div>;
+// 	} else if (props.tab == 2) {
+// 		return <div>내용2</div>;
+// 	}
+// }
+// 만약에 props를 쓰기 귀찮다 ? 다른 방법이 있는데 props 자리에 그냥 변수이름을 { }안에 넣으면 됨 위의 예시는 { tab } 이렇게 쓰면 개꿀이다
+//여러개가 있다? {tab, tab2, tab3} 이렇게 쓰면 됨
+//그리고 if문 대신에
+// [<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][props.tab]; 이렇게 사용하면 tab이 0번째면 내용0 1번째면 내용1을 꺼낼테니 if문이 필요없을 수도?
+export default DetailPage;
