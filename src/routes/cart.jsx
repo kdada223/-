b5befd334efcdcace1b4384283280d0e585f9974
kdada223 <kangh2580@naver.com>
2025-10-14 17:49:15 +0900

    redux값 조정해서 알아서 해보쇼

--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -1,6 +1,16 @@
 import { Table } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
 function Cart() {
+  let state = useSelector((state) => {
+    return state.item;
+  });
+  console.log(state.item);
+  //useSelector에서 익명함수 안의 state는 store안에 있는 모든 state를 뜻한다.
+  //그리고 나서 return state는 모든 state를 내보낸다는 뜻이지만 여기서 state.user 한 후 변수에 저장하면 그 변수에는 user에 대한 값만
+  //들어있다.
+  //그리고 redux를 쓸 때 주의할 점이 모든 state들을 store에 집어넣을 필요가 없음. 만약에 특정 컴포넌트 안에서만 쓰는 state면 그냥 거기서 만들면 됨
+
   return (
     <div>
       <Table>
--- src/store.jsx
+++ src/store.jsx
@@ -1,5 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+let user = createSlice({
+  name: 'user',
+  initialState: 'kang',
+});
+let stock = createSlice({
+  name: 'stock',
+  initialState: [10, 11, 12],
+});
+let item = createSlice({
+  item: [
+    { id: 0, name: 'White and Black', count: 2 },
+    { id: 2, name: 'Grey Yordan', count: 1 },
+  ],
+});
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    user: user.reducer,
+    stock: stock.reducer,
+    item: item.reducer,
+  },
 });
