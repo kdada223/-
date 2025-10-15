f6f8559f1be5d73b604a72dc86a5b3bfc5019a88
kdada223 <kangh2580@naver.com>
2025-10-15 23:20:25 +0900

    내일 숙제로 해오쇼

--- src/store.jsx
+++ src/store.jsx
@@ -1,24 +1,29 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
+import user from './store/userSlice.jsx';
 
-let user = createSlice({
-  name: 'user',
-  initialState: 'kang',
-});
 let stock = createSlice({
-  name: 'stock',
-  initialState: [10, 11, 12],
+	name: 'stock',
+	initialState: [10, 11, 12],
 });
+
 let item = createSlice({
-  item: [
-    { id: 0, name: 'White and Black', count: 2 },
-    { id: 2, name: 'Grey Yordan', count: 1 },
-  ],
+	name: 'item',
+	initialState: [
+		{ id: 0, name: 'White and Black', count: 2 },
+		{ id: 2, name: 'Grey Yordan', count: 1 },
+	],
+
+	reducers: {
+		
+	}
 });
+
+
 
 export default configureStore({
-  reducer: {
-    user: user.reducer,
-    stock: stock.reducer,
-    item: item.reducer,
-  },
+	reducer: {
+		user: user.reducer,
+		stock: stock.reducer,
+		item: item.reducer,
+	},
 });
---
+++ src/store/userSlice.jsx
@@ -0,0 +1,19 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+let user = createSlice({
+	name: 'user',
+	initialState: { name: 'kang', age: 20 },
+
+	reducers: {
+		changeName(state) {
+			state.name = 'Park';
+		},
+		changeAge(age, action) {
+			age.age += action.payload;
+		},
+	},
+});
+
+export let { changeName, changeAge } = user.actions;
+
+export default user;
